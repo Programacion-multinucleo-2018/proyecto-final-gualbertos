18c47945f1bbe703d72f4e23b7f7314e506ea5c9
Gual Casas <gualcm@teclag.com>
2018-11-23 20:39:14 -0600

    asdf

--- react/aStar.cu
+++ react/aStar.cu
@@ -92,7 +92,7 @@ __global__ void calcHeuristicOnGPU(float *heuristicMat, short rows, short cols, 
 void debugHeuristicMat(float heuristicMat, short rows, short cols) {
     for(int i = 0; i < rows; i++ ) {
         for(int j = 0; j < cols; j++ ) {
-            cout << "x: " << j << " y: " << i << "heuristic: " << heuristicMat[i][j] << endl;
+            cout << "x: " << j << " y: " << i << "heuristic: " << &heuristicMat[i][j] << endl;
         }
     }
 }
